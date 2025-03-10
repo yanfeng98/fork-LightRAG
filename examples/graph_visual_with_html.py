866e03d47dd72ee891d3c0e221ae6db2c5d94c54
LuYF-Lemon-love <3555028709@qq.com>
2025-03-10 17:59:34 +0800

    0000

--- examples/graph_visual_with_html.py
+++ examples/graph_visual_with_html.py
@@ -10,7 +10,7 @@ from pyvis.network import Network
 import random
 
 # Load the GraphML file
-G = nx.read_graphml("./dickens/graph_chunk_entity_relation.graphml")
+G = nx.read_graphml("./ZGF_Family_Letters/graph_chunk_entity_relation.graphml")
 
 # Create a Pyvis network
 net = Network(height="100vh", notebook=True)
@@ -31,4 +31,4 @@ for edge in net.edges:
         edge["title"] = edge["description"]
 
 # Save and display the network
-net.show("knowledge_graph.html")
+net.show("./ZGF_Family_Letters/knowledge_graph.html")
